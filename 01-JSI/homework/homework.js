636ae714261b6dfbca67dc2d06610fb1515160bc
erickmand <erickpadron@gmail.com>
2021-10-13 19:47:30 -0400

    finish homework 2

--- 01-JSI/homework/homework.js
+++ 01-JSI/homework/homework.js
@@ -1,22 +1,22 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta correcta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = null;
+const nuevaString = "Erick";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 1;
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - null === 5;
+const nuevaResta = 10 - 5 === 5;
 
 // Resuelve el siguiente problema matemático:
-const nuevaMultiplicacion = 10 * null === 40 ;
+const nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === null;
+const nuevoModulo = 21 % 5 === 21 % 5;
 
 
 // En los próximos 22 problemas, deberás completar la función.
@@ -28,77 +28,95 @@ const nuevoModulo = 21 % 5 === null;
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
-
+  return (x - y); 
 }
 
 function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-
+  if (x === y)
+    return true;
+  else 
+    return false;
 }
 
 function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-
+  if (str1.length === str2.length)
+    return true;
+   else   
+    return false; 
 }
 
 function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-
+  if (num < 90)
+    return true;
+   else
+    return false;  
 }
 
 function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-
+  if (num %2 === 0)
+    return false;
+   else
+    return true;
 }
 
 function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // Ojo: No es raiz cuadrada!
   // Tu código:
-
+  return Math.pow (num,2);
 }
 
 function elevar(num, exponent) {
   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
-  // Tu código:
-
-}
+  // Tu código: 
+  return Math.pow (num,exponent);
+  }
 
 function esPositivo(numero) {
   //La función va a recibir un entero. Devuelve como resultado una cadena de texto que indica si el número es positivo o negativo. 
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-
+  if (numero < 0)
+    return "Es negativo";
+  else if (numero === 0)
+   return false;
+     else
+      return "Es positivo";
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-
+  var combinado = nombre + " " + apellido;
+  return combinado;
 }
 
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   // Tu código:
-
+  return lado * 4;
 }
 
 function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   // Tu código:
-  
-}
+  return euro * 1.20;
+  }
 
 
 function esVocal(letra){
@@ -106,16 +124,26 @@ function esVocal(letra){
   //Verificar si el usuario ingresó un string de más de un carácter y, en ese caso, informarle 
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Tu código:
-
-}
+   if (letra.length > 1){
+    return "Dato incorrecto";
+  }
+    if (letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u")
+    return "Es vocal";
+      else
+        return "Dato incorrecto"
+        }
 
 function obtenerMayor(x, y) {
   // "x" e "y" son números enteros.
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
-
-}
+  if (x > y) {
+  return x;
+  }  else {
+    return y; 
+        }
+      }    
 
 /*
 Para implementar la siguiente función vamos a utilizar una estructura de control de flujo nueva, 
@@ -137,21 +165,38 @@ function colors(color) {
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
   // Tu código:
-
-}
+  switch (color) {
+      case "blue":
+        return "This is blue";
+      case "red":
+        return "This is red";
+      case"green":
+        return "This is green";
+      case "orange":
+        return "This is orange";
+      default:
+        return "Color not found";
+    }    
+    }
 
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
-
-}
+  if (numero === 5 || numero === 10)
+  return true;
+   else 
+    return false;
+  }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
-
+  if (numero < 50 && numero > 20)
+  return true;
+    else  
+      return false;
 }
 
 function esPrimo(numero) {
@@ -161,15 +206,29 @@ function esPrimo(numero) {
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
   // Tu código:
+    if (numero === 0 || numero === 1) {
+       return false;
+    }
 
-}
+      for (var i = 2; i < numero; i++) {
+        if(numero % i === 0) {
+        return false; 
+        }
+    }
+  return true;
+ }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Pueden usar un while o un bucle do ... while (Investigarlo si quieren hacerlo de la segunda forma)
   // Tu código:
-  
+  var i = 0;
+    while (i < 8) {
+      numero = numero + 5;
+      i ++;
+    }
+      return numero;
 }
 
 
